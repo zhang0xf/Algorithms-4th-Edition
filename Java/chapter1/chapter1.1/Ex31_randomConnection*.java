7e2d268441bee57d7003ed2511a8ca7b3b4b4531
张飞 <861736862@qq.com>
2019-08-13 15:01:17 +0800

    Create Ex31_randomConnection*.java

---
+++ Java/chapter1/chapter1.1/Ex31_randomConnection*.java
@@ -0,0 +1,25 @@
+/* *****************************************************************************
+ *  Name:    Alan Turing
+ *  NetID:   aturing
+ *  Precept: P00
+ *
+ *  Description:  Prints 'Hello, World' to the terminal window.
+ *                By tradition, this is everyone's first program.
+ *                Prof. Brian Kernighan initiated this tradition in 1974.
+ *
+ **************************************************************************** */
+
+// random connection
+
+// difficult and deal later.
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+
+public class Ex31 {
+    public static void main(String[] args) {
+        int n = parseInt(args[0]);
+        double p = parseDouble(args[1]);
+
+    }
+}

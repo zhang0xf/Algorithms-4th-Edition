4b8990e14cacc2cdb3cc7ef166e4f9c7ecce052c
张飞 <861736862@qq.com>
2020-07-02 17:33:40 +0800

    Create Ex1.3.38_2_GeneralizedQueueBaseOnLinkList.java

---
+++ Java/chapter1/chapter1.3/Ex1.3.38_2_GeneralizedQueueBaseOnLinkList.java
@@ -0,0 +1,98 @@
+/* *****************************************************************************
+ *  Name:    Alan Turing
+ *  NetID:   aturing
+ *  Precept: P00
+ *
+ *  Description:  Prints 'Hello, World' to the terminal window.
+ *                By tradition, this is everyone's first program.
+ *                Prof. Brian Kernighan initiated this tradition in 1974.
+ *
+ **************************************************************************** */
+
+// base on Linklist
+
+public class GeneralizedQueueBaseOnLinkList<Item> {
+
+    private int N;
+    private Node first;
+    private Node last;
+
+    public class Node {
+        Item item;
+        Node next;
+    }
+
+    public GeneralizedQueueBaseOnLinkList(int max) {
+        // first.item = null;
+        // first.next = null;
+        // last.item = null;
+        // last.next = null;
+        first = null;
+        last = null;
+        N = 0;
+    }
+
+    public boolean isEmpty() {
+        return 0 == N;
+    }
+
+    public void insert(Item item) {
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        }
+        else {
+            oldlast.next = last;
+        }
+        N++;
+    }
+
+    public Item delete(int k) {
+        // assuming start at 0
+        Node curr = first;
+        int cnt = k - 1 - 1;
+        while (curr != null && cnt > 0) {
+            curr = curr.next;
+            cnt--;
+        }
+        if (curr.next == null) {
+            return null;
+        }
+        Item temp = curr.next.item;
+        curr.next = curr.next.next;
+        N--;
+        return temp;
+    }
+
+    public void printQueue() {
+        Node pCurr = first;
+        while (pCurr != null) {
+            StdOut.print(pCurr.item + " ");
+            pCurr = pCurr.next;
+        }
+        StdOut.println();
+    }
+
+    public static void main(String[] args) {
+
+        GeneralizedQueueBaseOnLinkList<Integer> queue
+                = new GeneralizedQueueBaseOnLinkList<Integer>(10);
+
+        queue.insert(1);
+        queue.insert(2);
+        queue.insert(3);
+        queue.insert(4);
+        queue.insert(5);
+        queue.insert(6);
+        queue.printQueue();
+
+        StdOut.println(queue.delete(2) + " be deleted ");
+        queue.printQueue();
+
+        StdOut.println(queue.delete(3) + " be deleted ");
+        queue.printQueue();
+    }
+}

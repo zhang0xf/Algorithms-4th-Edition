7c2d7541c4518c3658d371f714e05db8a9aadcae
张飞 <861736862@qq.com>
2020-07-23 15:27:02 +0800

    Create Ex1.5.2_QuickUnio.java

---
+++ Java/chapter1/chapter1.5/Ex1.5.2_QuickUnio.java
@@ -0,0 +1,71 @@
+/* *****************************************************************************
+ *  Name:    Alan Turing
+ *  NetID:   aturing
+ *  Precept: P00
+ *
+ *  Description:  Prints 'Hello, World' to the terminal window.
+ *                By tradition, this is everyone's first program.
+ *                Prof. Brian Kernighan initiated this tradition in 1974.
+ *
+ **************************************************************************** */
+
+// with file tinyUf.txt mediumUf.txt largeUf.txt
+
+public class QuickUnio {
+
+    private int[] id;
+    private int count;
+
+    public QuickUnio(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+        count = N;
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) return;
+        id[pRoot] = qRoot;
+        count--;
+    }
+
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        QuickFind QU = new QuickFind(N);
+
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+
+            if (QU.connected(p, q)) {
+                continue;
+            }
+
+            QU.union(p, q);
+            StdOut.println(p + " -> " + q);
+            QU.printArray();
+            // StdOut.println("access to array times total : " + QU.times());
+        }
+
+        StdOut.println(QU.count() + " components ");
+    }
+}
